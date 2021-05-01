9fd1a1f1f177478ccbe7cef5d901d36d3ff32eb7
anithasamuel-JA <80707575+anithasamuel-JA@users.noreply.github.com>
2021-05-01 12:06:54 +0530

    Update DeleteLead.java

--- assignments/week2/day2/DeleteLead.java
+++ assignments/week2/day2/DeleteLead.java
@@ -8,26 +8,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class DeleteLead {
 
 	public static void main(String[] args) throws InterruptedException {
-		// TODO Auto-generated method stub
-//		1	Launch the browser
-//		2	Enter the username
-//		3	Enter the password
-//		4	Click Login
-//		5	Click crm/sfa link
-//		6	Click Leads link
-//		7	Click Find leads
-//		8	Click on Phone
-//		9	Enter phone number
-//		10	Click find leads button
-//		11	Capture lead ID of First Resulting lead
-//		12	Click First Resulting lead
-//		13	Click Delete
-//		14	Click Find leads
-//		15	Enter captured lead ID
-//		16	Click find leads button
-//		17	Verify message "No records to display" in the Lead List. This message confirms the successful deletion
-//		18	Close the browser (Do not log out)
-//	
+		// TODO Auto-generated method stub	
 		
 		WebDriverManager.chromedriver().setup();
 		ChromeDriver driver = new ChromeDriver();
@@ -59,19 +40,20 @@ public class DeleteLead {
 		driver.findElement(By.name("phoneNumber")).sendKeys("9988776655");
 
 		//click on find button
-		driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
-		
-		//capture first id
-		String LeadID = driver.findElement(By.xpath("//div[@class='x-grid3-cell-inner x-grid3-col-partyId']//a")).getText();
-		System.out.println("LeadID : " + LeadID);
-		Thread.sleep(1000);
-		
-		//click First Resulting lead
-		driver.findElement(By.xpath("//div[@class='x-grid3-cell-inner x-grid3-col-partyId']//a")).click();;
-		Thread.sleep(1000);
-		
-		//click Delete
-		driver.findElement(By.linkText("Delete"));
+        driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
+        Thread.sleep(1000);
+
+        //capture first id
+        String LeadID = driver.findElement(By.xpath("//div[@class='x-grid3-cell-inner x-grid3-col-partyId']//a")).getText();
+        System.out.println("LeadID : " + LeadID);
+        Thread.sleep(1000);
+
+        //click First Resulting lead
+        driver.findElement(By.xpath("//div[@class='x-grid3-cell-inner x-grid3-col-partyId']//a")).click();;
+        Thread.sleep(1000);
+
+        //click Delete
+        driver.findElement(By.linkText("Delete")).click();
         
 		//click on find leads
 		driver.findElement(By.linkText("Find Leads")).click();
@@ -80,19 +62,16 @@ public class DeleteLead {
 		driver.findElement(By.name("id")).sendKeys(LeadID);
 
 		//click on find button
-		driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
-        
-        	boolean status = driver.findElement(By.xpath("//div[text()='No records to display']")).isDisplayed();
+		
+        driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
+        boolean status = driver.findElement(By.xpath("//div[text()='No records to display']")).isDisplayed();
 
-        	if (status) {
-        		System.out.println("This message confirms the successful deletion of the mentioned Lead Id");
+        if (status) {
+        	System.out.println("This message confirms the successful deletion of the mentioned Lead Id");
 		} 
         
-        	else {
+        else {
 			System.err.println("Deletion step failed");
 		}
-		
-        	driver.close();
 	}
-
 }
