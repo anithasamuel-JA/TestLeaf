9ce7981775c25ea2f7107b372d94d01282c75339
anithasamuel-JA <80707575+anithasamuel-JA@users.noreply.github.com>
2021-05-15 13:48:07 +0530

    Update DeleteChangeRequest.java

--- assignments/Week4/Day2/Change Management/DeleteChangeRequest.java
+++ assignments/Week4/Day2/Change Management/DeleteChangeRequest.java
@@ -1,8 +1,5 @@
 package change_Management;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -13,7 +10,7 @@ import org.openqa.selenium.support.ui.Select;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class Assess_the_Change_Request {
+public class DeleteChangeRequest {
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
@@ -23,6 +20,8 @@ public class Assess_the_Change_Request {
 		ChromeDriver driver =new ChromeDriver(); 
 
 		driver.get("https://dev103117.service-now.com");
+
+
 
 		driver.manage().window().maximize();
 
@@ -47,6 +46,9 @@ public class Assess_the_Change_Request {
 
 		driver.switchTo().frame("gsft_main");
 		Thread.sleep(2000);
+		
+		WebElement forTextNum = driver.findElement(By.xpath("//select[@class='form-control default-focus-outline']")); 
+		new Select(forTextNum).selectByIndex(1);
 
 		WebElement searchField=driver.findElement(By.xpath("//input[@class='form-control']"));
 		searchField.sendKeys("CHG0030626");
@@ -56,80 +58,19 @@ public class Assess_the_Change_Request {
 		driver.findElement(By.linkText("CHG0030626")).click();
 		Thread.sleep(2000);
 
-		WebElement incidentstate = driver.findElement(By.id("change_request.state")); 
-		Select state2=new Select(incidentstate);
-		state2.selectByIndex(1);
-		Thread.sleep(2000);
-		WebElement stateText = state2.getFirstSelectedOption();
-		String state=stateText.getText();
-		System.out.println(state);
+		driver.findElement(By.id("sysverb_delete")).click();
+
+		driver.findElement(By.id("ok_button")).click();
 
 		Thread.sleep(2000);
 
-		driver.findElement(By.xpath("//button[@id='lookup.change_request.assignment_group']//span[1]")).click();
+		String noRecordsTo = driver.findElement(By.xpath("//td[text()='No records to display']")).getText();
 
-		Thread.sleep(2000);
-
-		Set<String> newWindow=driver.getWindowHandles();
-		List<String> allWindow=new ArrayList<String>(newWindow);
-
-		String secondWindow=allWindow.get(1);
-
-		driver.switchTo().window(secondWindow);
-
-		driver.findElement(By.linkText("Software")).click();
-
-		driver.switchTo().window(allWindow.get(0));	
-
-		driver.switchTo().frame("gsft_main");
-		Thread.sleep(2000);
-
-		driver.findElement(By.xpath("//button[@data-for='sys_display.change_request.assigned_to']//span[1]")).click();
-
-		Thread.sleep(2000);
-
-		newWindow=driver.getWindowHandles();
-		allWindow=new ArrayList<String>(newWindow);
-
-		secondWindow=allWindow.get(1);
-		driver.switchTo().window(secondWindow);
-
-		Thread.sleep(2000);
-
-		driver.findElement(By.linkText("ITIL User")).click();
-		driver.switchTo().window(allWindow.get(0));	
-		Thread.sleep(2000);
-
-		driver.switchTo().frame("gsft_main");
-		Thread.sleep(2000);
-
-		driver.findElement(By.id("sysverb_update")).click();
-		
-		String state1=driver.findElement(By.xpath("//td[text()='Assess']")).getText();
-
-		if(state.equals(state1))
+		if(noRecordsTo.equals("No records to display"))
 		{
-			System.out.println("State Equals");
-		}
-		else
-		{
-			System.out.println("State Not Equals");
+			System.out.println("Deleted Successfully");
 		}
 
-		String incidentAssignedTo = driver.findElement(By.linkText("ITIL User")).getText();
-		System.out.println(incidentAssignedTo);
-		
-		if(incidentAssignedTo.equals("ITIL User"))
-		{
-			System.out.println("Assigned To is verified");
-		}
-		else
-		{
-			System.out.println("Assigned To is not verified");
-		}
-
-		driver.switchTo().defaultContent();
-		
 		driver.close();
 
 	}
