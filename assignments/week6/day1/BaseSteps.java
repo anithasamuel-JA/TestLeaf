0ab491620415fb4c981ba8ca06675b1971127752
anithasamuel-JA <80707575+anithasamuel-JA@users.noreply.github.com>
2021-05-23 13:25:53 +0530

    Create BaseSteps.java

---
+++ assignments/week6/day1/BaseSteps.java
@@ -0,0 +1,11 @@
+package StepDefinition;
+
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseSteps {
+
+	public static ChromeDriver driver;
+	// public static WebDriverWait wait;
+
+}
