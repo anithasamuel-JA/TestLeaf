7b2d645a0adc59c1ddc60ace005f8027f5f167b4
anithasamuel-JA <80707575+anithasamuel-JA@users.noreply.github.com>
2021-05-29 13:17:33 +0530

    Create CreateIncident.java

---
+++ assignments/week6/day2/pom/tests/CreateIncident.java
@@ -0,0 +1,19 @@
+package assignments.week6.day2.pom.tests;
+
+import org.testng.annotations.Test;
+
+import week6.day2.assignments.hooks.TestNgHooks;
+import week6.day2.assignments.pages.ServiceLoginPage;
+
+public class CreateIncident extends TestNgHooks {
+
+	@Test
+	public void createIncidentTest() throws InterruptedException {
+
+		new ServiceLoginPage().typeUName().typePwd().clickLoginButton().filterNavigatorIncident().clickNewButton()
+				.getIncNumber().clickCallerInfoLookup().assignUserValue().assignShortDesc().clickOnSubmitButton()
+				.verifyCreatedIncident();
+
+	}
+
+}
