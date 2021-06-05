50ed41ad58ceda2ee4cd0577efed39216ddeccd4
anithasamuel-JA <80707575+anithasamuel-JA@users.noreply.github.com>
2021-06-05 13:58:31 +0530

    Create TestNgHooks.java

---
+++ assignments/week7/day2/hooks/TestNgHooks.java
@@ -0,0 +1,36 @@
+package week7.day2.assignments.hooks;
+
+import java.io.IOException;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+
+import week7.day2.assignments.utils.DataLibrary;
+import week7.day2.assignments.wrappers.BaseDriver;
+
+public class TestNgHooks extends BaseDriver {
+
+	public static ChromeDriver driver;
+	public static String number;
+
+	public String dataSheetName;
+
+	@DataProvider(name = "FetchExcelData")
+	public Object[][] fetchData() throws IOException, InvalidFormatException {
+		return DataLibrary.readExcelData(dataSheetName);
+	}
+
+	@BeforeMethod
+	public void beforeMethod() {
+		startApp("chrome", "https://dev103117.service-now.com");
+		setNode();
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+		close();
+	}
+}
