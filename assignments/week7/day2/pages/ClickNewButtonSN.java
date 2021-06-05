3ab629a956223baf30111d5e97309846156909ca
anithasamuel-JA <80707575+anithasamuel-JA@users.noreply.github.com>
2021-06-05 13:58:57 +0530

    Create ClickNewButtonSN.java

---
+++ assignments/week7/day2/pages/ClickNewButtonSN.java
@@ -0,0 +1,11 @@
+package week7.day2.assignments.pages;
+
+import week7.day2.assignments.hooks.TestNgHooks;
+
+public class ClickNewButtonSN extends TestNgHooks {
+
+	public CreateCallerPagesSN clickNewButton() {
+
+		return new CreateCallerPagesSN();
+	}
+}
